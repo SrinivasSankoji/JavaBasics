7bb6bdf7b7dd58ef8f74203757f814ac7203e295
srinivassankoji <srinivaschary.chary@gmail.com>
2021-06-18 20:44:35 +0530

    Java Interview

--- DateTest/src/main/java/com/example/demo/StringSplit.java
+++ DateTest/src/main/java/com/example/demo/StringSplit.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 public class StringSplit {
 	public static void main(String[] args) {
-		String strMain = "randomFromList(merchantCode_SIT)";
+		/**String strMain = "randomFromList(merchantCode_SIT)";
 		String randomFromList = strMain.split("randomFromList")[1];
 		String randomFromValue=randomFromList.substring(1, randomFromList.length()-1);
-		System.out.println(randomFromValue);
+		System.out.println(randomFromValue);**/
+		
+		String message="Suite à votre import, si vous ne recevez pas un email de CALIS,écrivez à list.alerte-cam@renault.com Chargement du fichier effectuï¿½";
+		String[] randomFromList = message.split(",");
+		String firstLine=randomFromList[0].concat(randomFromList[1]);
+		String[] newMessage=randomFromList[2].split(".com");
+		String secondLine=newMessage[0];
+		String thirdLine=newMessage[1];
+		System.out.println(firstLine);
+		System.out.println(secondLine);
+		System.out.println(thirdLine);
 	}
 }
