4b6db5145a93a0de36aa955964684e11405bb6d0
srinivassankoji <srinivaschary.chary@gmail.com>
2021-08-19 23:50:26 +0530

    Serialization

--- Exception/src/main/java/com/jio/ngo/common/RequestBody.java
+++
@@ -1,7 +0,0 @@
-package com.jio.ngo.common;
-
-public class RequestBody 
-{
-	
-
-}
--- Exception/src/main/java/com/jio/ngo/common/StaticVariables.java
+++
@@ -1,9 +0,0 @@
-package com.jio.ngo.common;
-
-import java.sql.Connection;
-import java.util.LinkedHashMap;
-
-public class StaticVariables {
-
-	public static LinkedHashMap<String,Connection> connMap = null;;
-}
