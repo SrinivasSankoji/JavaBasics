48ae7830d82a70b7c154d395aa9611857f51b42b
srinivassankoji <srinivaschary.chary@gmail.com>
2026-03-07 15:56:59 +0530

    Updated to java 17

--- Java8/src/main/java/com/jio/ngo/comparator/ComparatorAfterJava8.java
+++ Java8/src/main/java/com/jio/ngo/comparator/ComparatorAfterJava8.java
@@ -4,31 +4,27 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ComparatorAfterJava8 {
-	static List<Employee> employeeList = Arrays.asList(new Employee(10, "Mikey", 25, 10000),
-			new Employee(20, "Arun", 29, 20000), new Employee(5, "Lisa", 35, 5000),
-			new Employee(1, "Pankaj", 32, 50000));
+    static List<Employee> employeeList = Arrays.asList(new Employee(10, "Mikey", 25, 10000),
+            new Employee(20, "Arun", 29, 20000), new Employee(5, "Lisa", 35, 5000),
+            new Employee(1, "Pankaj", 32, 50000));
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/**
-		 * Comparator using Lambda Expression
-		 */
-		Comparator<Employee> nameComparator = (employee1, employee2) -> employee1.getName().compareTo(employee2.getName());
+        /**
+         * Comparator using Lambda Expression
+         */
+        Comparator<Employee> nameComparator = (employee1, employee2) -> employee1.getName().compareTo(employee2.getName());
 
-		/**
-		 * Comparator using Method Reference
-		 */
-		//Collections.sort(employeeList, Comparator.comparing(Employee::getName));
-		Collections.sort(employeeList,Comparator.comparing(Employee::getSalary).reversed()); // Reverse Order using Collections Class sort().
-		//Collections.sort(employeeList, Comparator.comparing(Employee::getId).thenComparing(Employee::getName));
-		employeeList.sort(Comparator.comparing(Employee::getName).thenComparing(Employee::getName));// Reverse Order using List Interface Class sort().
-		//employeeList.stream().limit(2).forEach(System.out::println);
-		//employeeList.stream().forEach(System.out::println);
-		
-		employeeList.forEach(System.out::println);
-
-	}
+        /**
+         * Comparator using Method Reference
+         */
+        //Collections.sort(employeeList, Comparator.comparing(Employee::getName));
+        Collections.sort(employeeList, Comparator.comparing(Employee::getSalary).reversed()); // Reverse Order using Collections Class sort().
+        Collections.sort(employeeList, Comparator.comparing(Employee::getId).thenComparing(Employee::getName));
+        employeeList.sort(Comparator.comparing(Employee::getName).thenComparing(Employee::getName));// Reverse Order using List Interface Class sort().
+        employeeList.stream().limit(2).forEach(System.out::println);
+        employeeList.forEach(System.out::println);
+    }
 }
